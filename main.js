ccc3b5b2435ef443bea00a3e6987e108a583b29a
Shivam Chhetri <34092080+ShivamChhetri@users.noreply.github.com>
2019-09-06 16:00:26 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,61 @@
+let devices=document.getElementById('devices');
+class GenericDevice {
+
+    constructor() {
+      this.device = null;
+      this.onDisconnected = this.onDisconnected.bind(this);
+    }
+    
+    request() {
+      let options = {
+        "acceptAllDevices ": true,
+        // "optionalServices": ["device_information"]
+      };
+      return navigator.bluetooth.requestDevice(options)
+      .then(device => {
+        this.device = device;
+        this.device.addEventListener('gattserverdisconnected', this.onDisconnected);
+      });
+    }
+    
+    connect() {
+      if (!this.device) {
+        return Promise.reject('Device is not connected.');
+      }
+      return this.device.gatt.connect();
+    }
+    dispDetail(){
+        devices.innerHTML=`name:${device.name}\n id:${device.id}\n connected:${device.gatt.connected}`;
+        return;
+        // return this.device;
+    }
+    readManufacturername() {
+      return this.device.gatt.getPrimaryService("device_information")
+      .then(service => service.getCharacteristic("manufacturer_name_string"))
+      .then(characteristic => characteristic.readValue());
+    }
+  
+    disconnect() {
+      if (!this.device) {
+        return Promise.reject('Device is not connected.');
+      }
+      return this.device.gatt.disconnect();
+    }
+  
+    onDisconnected() {
+      console.log('Device is disconnected.');
+    }
+  }
+  
+  var genericDevice = new GenericDevice();
+  
+  document.querySelector('button').addEventListener('click', event => {
+    genericDevice.request()
+    .then(_ => genericDevice.connect())
+    .then(_ => {
+        genericDevice.dispDetail();
+        // let device= genericDevice.dispDetail();
+        // devices.innerHTML=`name:${device.name}\n id:${device.id}\n connected:${device.gatt.connected}`;
+    })
+    .catch(error => { console.log(error) });
+  });
